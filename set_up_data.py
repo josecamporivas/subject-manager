8b351604a597a0f5f966066f72f429c1232cd16d
Jose Campo Rivas <josecampor31@gmail.com>
2024-04-22 17:28:54 +0200

    Update set_up_data script

--- set_up_data.py
+++ set_up_data.py
@@ -1,22 +1,35 @@
 import sirope
 
 from models.Teacher import Teacher
+from models.Subject import Subject
 
 srp = sirope.Sirope()
 
 def create_teachers():
     teachers = [
-        Teacher('John Doe', 'john.doe', 'password'),
-        Teacher('Jane Doe', 'jane.doe', 'password'),
-        Teacher('John Smith', 'john.smith', 'password'),
-        Teacher('Jane Smith', 'jane.smith', 'password'),
-        Teacher('John Johnson', 'john.johnson', 'password'),
-        Teacher('Jane Johnson', 'jane johnson', 'password')
+        Teacher('Juan Carlos', 'juanca', 'juanca', 'Math'),
+        Teacher('Antonio Recio', 'antoniorecio', 'password', 'Spanish'),
+        Teacher('Marcial', 'marcial', 'password', 'Physical Education'),
+        Teacher('Eugenio', 'eugenio', 'password', 'Music'),
+        Teacher('Ramon', 'ramon', 'password', 'Science'),
+        Teacher('Rafa Nadal', 'rafa', 'password', 'Physical Education')
     ]
 
     for teacher in teachers:
         srp.save(teacher)
 
+def create_subjects():
+    subjects = [
+        Subject('Math', 'Laboratory 1'),
+        Subject('Spanish', 'Laboratory 2'),
+        Subject('Physical Education', 'Gym'),
+        Subject('Music', 'Music Room'),
+        Subject('Science', 'Laboratory 3'),
+    ]
+
+    for subject in subjects:
+        srp.save(subject)
 
 if __name__ == '__main__':
     create_teachers()
+    create_subjects()
